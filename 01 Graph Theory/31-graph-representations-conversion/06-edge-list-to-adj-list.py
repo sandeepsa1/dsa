f1ebf2a1edbc39e12870d66deea1d9e5da4ad75b
sandeepsa1 <sandeepsasidharan@gmail.com>
2024-12-26 22:28:24 +0530

    Converting between different graph representations

--- 01 Graph Theory/31-graph-representations-conversion/06-edge-list-to-adj-list.py
+++ 01 Graph Theory/31-graph-representations-conversion/06-edge-list-to-adj-list.py
@@ -9,4 +9,4 @@ def edge_list_to_adj_list(edge_list, n):
 
 
 edge_list = [(0, 1), (1, 2), (1, 3)]
-print(edge_list_to_adj_list(edge_list, 4)) # {0: [1], 1: [0, 2, 3], 2: [1], 3: [1]} 
+print(edge_list_to_adj_list(edge_list, 4)) # {0: [1], 1: [0, 2, 3], 2: [1], 3: [1]}
