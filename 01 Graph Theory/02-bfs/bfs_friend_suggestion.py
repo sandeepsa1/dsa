8a892c9055d39802a68352a839b45663287e5558
sandeepsa1 <sandeepsasidharan@gmail.com>
2025-07-23 23:14:04 +0530

    BFS based friend suggestion

--- 01 Graph Theory/02-bfs/bfs_friend_suggestion.py
+++ 01 Graph Theory/02-bfs/bfs_friend_suggestion.py
@@ -11,6 +11,20 @@ def suggest_friends(graph, user):
     visited.add(user)
     level[user] = 0
 
+    while queue:
+        current, depth = queue.popleft()
+        if depth > 2:
+            break
+        for neighbor in graph.get(current, []):
+            if neighbor not in visited:
+                visited.add(neighbor)
+                level[neighbor] = depth + 1
+                queue.append((neighbor, depth + 1))
+                if level[neighbor] == 2 and neighbor not in graph[user]:
+                    # Count mutual connections
+                    mutuals = set(graph[user]).intersection(set(graph[neighbor]))
+                    suggestions[neighbor] = len(mutuals)
+
     return dict(sorted(suggestions.items(), key=lambda x: -x[1]))  # sort by most mutuals
 
 
