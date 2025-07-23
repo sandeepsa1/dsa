8116da634f73ad62f18c9536204f822697924571
sandeepsa1 <sandeepsasidharan@gmail.com>
2025-07-23 22:40:50 +0530

    BFS based friend suggestion

--- 01 Graph Theory/02-bfs/bfs_friend_suggestion.py
+++ 01 Graph Theory/02-bfs/bfs_friend_suggestion.py
@@ -1,4 +1,17 @@
 # 'A' is friends with 'B' and 'C'. Suggest friends for 'A'
+from collections import deque, defaultdict
+
+def suggest_friends(graph, user):
+    visited = set()
+    queue = deque()
+    level = {}
+    suggestions = defaultdict(int)
+
+    queue.append((user, 0))
+    visited.add(user)
+    level[user] = 0
+
+    return dict(sorted(suggestions.items(), key=lambda x: -x[1]))  # sort by most mutuals
 
 
 graph = {
