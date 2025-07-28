9ce2419a93abd5c750cba8125cb6f1369aba310a
sandeepsa1 <sandeepsasidharan@gmail.com>
2025-07-28 23:47:21 +0530

    Find Connected Components using BFS

--- 01 Graph Theory/02-bfs/bfs_connected_components.py
+++ 01 Graph Theory/02-bfs/bfs_connected_components.py
@@ -1,3 +1,29 @@
+from collections import deque
+
+def find_connected_components_bfs(graph):
+    visited = set()
+    components = []
+
+    for node in graph:
+        if node not in visited:
+            component = []
+            queue = deque([node])
+            visited.add(node)
+
+            while queue:
+                current = queue.popleft()
+                component.append(current)
+
+                for neighbor in graph[current]:
+                    if neighbor not in visited:
+                        visited.add(neighbor)
+                        queue.append(neighbor)
+
+            components.append(component)
+    
+    return components
+
+
 graph = {
     'A': ['B'],
     'B': ['A', 'C'],
@@ -8,4 +34,4 @@ graph = {
 }
 
 components = find_connected_components_bfs(graph)
-print("Connected Components:", components)
+print("Connected Components:", components) # [['A', 'B', 'C'], ['D', 'E'], ['F']]
