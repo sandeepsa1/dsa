f50ba998f25a5e69bb1d4fd8cb00171ce4419f5d
sandeepsa1 <sandeepsasidharan@gmail.com>
2025-08-21 23:03:56 +0530

    Minimum operations required to transform a number to another

--- 01 Graph Theory/02-bfs/bfs_min_steps_to_transform_number.py
+++ 01 Graph Theory/02-bfs/bfs_min_steps_to_transform_number.py
@@ -1,5 +1,21 @@
 from collections import deque
 
 def min_operations_number(start, target):
+    queue = deque([(start, 0)])  # (number, steps)
+    visited = set([start])
+
+    while queue:
+        num, steps = queue.popleft()
+        if num == target:
+            return steps
+
+        # allowed moves
+        for next_num in (num - 1, num + 1, num * 2):
+            if 0 <= next_num <= 10000 and next_num not in visited:
+                visited.add(next_num)
+                queue.append((next_num, steps + 1))
     
     return -1
+
+
+print(min_operations_number(4, 17)) # 4  → 8  → 16 → 17 (3 steps)
