8dfe2c9e5efbb5252adf16d307122618dd30dab3
sandeepsa1 <sandeepsasidharan@gmail.com>
2025-09-11 00:22:20 +0530

    BFS based flood fill

--- 01 Graph Theory/02-bfs/bfs_flood_fill.py
+++ 01 Graph Theory/02-bfs/bfs_flood_fill.py
@@ -1,5 +1,28 @@
 from collections import deque
 
+def flood_fill_bfs(image, sr, sc, new_color):
+    rows, cols = len(image), len(image[0])
+    original_color = image[sr][sc]
+    if original_color == new_color:
+        return image  # nothing to change
+
+    queue = deque([(sr, sc)])
+    image[sr][sc] = new_color  # fill starting pixel
+
+    directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]  # up, down, left, right
+
+    while queue:
+        r, c = queue.popleft()
+
+        for dr, dc in directions:
+            nr, nc = r + dr, c + dc
+            if 0 <= nr < rows and 0 <= nc < cols and image[nr][nc] == original_color:
+                image[nr][nc] = new_color
+                queue.append((nr, nc))
+
+    return image
+
+
 image = [
     [1, 1, 0, 2],
     [1, 0, 0, 2],
@@ -8,3 +31,9 @@ image = [
 
 sr, sc = 0, 0  # starting point
 new_color = 9
+
+filled_image = flood_fill_bfs(image, sr, sc, new_color)
+for row in filled_image:
+    print(row)                  # [9, 9, 0, 2]
+                                # [9, 0, 0, 2]
+                                # [9, 9, 0, 2]
