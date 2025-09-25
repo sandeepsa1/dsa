d39109f1dbcf4cabb36cb149297ec02a1fea8111
sandeepsa1 <sandeepsasidharan@gmail.com>
2025-09-25 19:27:11 +0530

    BFS based Knights Shortest Path

---
+++ 01 Graph Theory/02-bfs/bfs_knights_shortest_path.py
@@ -0,0 +1,11 @@
+from collections import deque
+
+def knight_shortest_path(N, start, target):
+    return -1
+
+
+N = 8  # 8x8 chessboard
+start = (0, 0)
+target = (7, 7)
+
+print("Minimum knight moves:", knight_shortest_path(N, start, target))
