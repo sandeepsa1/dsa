ae3c6246bf593acb468270c1c8ae7678369675c0
sandeepsa1 <sandeepsasidharan@gmail.com>
2025-09-28 14:11:01 +0530

    BFS based Knights Shortest Path

--- 01 Graph Theory/02-bfs/bfs_knights_shortest_path.py
+++ 01 Graph Theory/02-bfs/bfs_knights_shortest_path.py
@@ -1,7 +1,29 @@
 from collections import deque
 
 def knight_shortest_path(N, start, target):
-    return -1
+    # All 8 possible knight moves
+    moves = [
+        (2, 1), (1, 2), (-1, 2), (-2, 1),
+        (-2, -1), (-1, -2), (1, -2), (2, -1)
+    ]
+    
+    visited = set()
+    queue = deque([(start[0], start[1], 0)])  # (row, col, distance)
+    visited.add(start)
+    
+    while queue:
+        r, c, dist = queue.popleft()
+        
+        if (r, c) == target:
+            return dist  # Found shortest path
+        
+        for dr, dc in moves:
+            nr, nc = r + dr, c + dc
+            if 0 <= nr < N and 0 <= nc < N and (nr, nc) not in visited:
+                visited.add((nr, nc))
+                queue.append((nr, nc, dist + 1))
+    
+    return -1  # No path (shouldn't happen on chessboard)
 
 
 N = 8  # 8x8 chessboard
