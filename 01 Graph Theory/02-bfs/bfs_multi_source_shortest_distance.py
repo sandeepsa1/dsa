4b63294cae8e3b61ecf738645136cc66b9d6c0eb
sandeepsa1 <sandeepsasidharan@gmail.com>
2025-10-01 23:12:41 +0530

    BFS based Shortest Distance from Multiple Sources Simultaneously

---
+++ 01 Graph Theory/02-bfs/bfs_multi_source_shortest_distance.py
@@ -0,0 +1,16 @@
+from collections import deque
+
+def multi_source_bfs(matrix):
+    
+    return [1,2]
+
+
+matrix = [
+    [0, 0, 1],
+    [0, 0, 0],
+    [1, 0, 0]
+]
+
+distances = multi_source_bfs(matrix)
+for row in distances:
+    print(row)
