38bb557bcd6b732d6cf5233f645694699fd989e2
sandeepsa1 <sandeepsasidharan@gmail.com>
2025-11-06 14:59:21 +0530

    BFS Based Minesweeper

--- 01 Graph Theory/02-bfs/bfs_mine_sweeper.py
+++ 01 Graph Theory/02-bfs/bfs_mine_sweeper.py
@@ -9,3 +9,64 @@
 #   If it’s a mine, it becomes 'X'.
 #   If it’s an empty cell with adjacent mines → show the count.
 #   If it’s an empty cell with no adjacent mines, reveal it as 'B' and recursively reveal all its neighbors (BFS/DFS).
+
+
+from collections import deque
+
+def updateBoard(board, click):
+    rows, cols = len(board), len(board[0])
+    r, c = click
+
+    # If mine clicked
+    if board[r][c] == 'M':
+        board[r][c] = 'X'
+        return board
+
+    directions = [(-1, -1), (-1, 0), (-1, 1),
+                  (0, -1),           (0, 1),
+                  (1, -1),  (1, 0),  (1, 1)]
+
+    def count_adjacent_mines(r, c):
+        count = 0
+        for dr, dc in directions:
+            nr, nc = r + dr, c + dc
+            if 0 <= nr < rows and 0 <= nc < cols and board[nr][nc] == 'M':
+                count += 1
+        return count
+
+    queue = deque([click])
+
+    while queue:
+        r, c = queue.popleft()
+        if board[r][c] != 'E':
+            continue  # already revealed
+
+        mines = count_adjacent_mines(r, c)
+        if mines > 0:
+            board[r][c] = str(mines)
+        else:
+            board[r][c] = 'B'
+            for dr, dc in directions:
+                nr, nc = r + dr, c + dc
+                if 0 <= nr < rows and 0 <= nc < cols and board[nr][nc] == 'E':
+                    queue.append((nr, nc))
+
+    return board
+
+
+board = [
+    ['E', 'E', 'E', 'E', 'E'],
+    ['E', 'E', 'M', 'E', 'E'],
+    ['E', 'E', 'E', 'E', 'E'],
+    ['E', 'E', 'E', 'E', 'E']
+]
+
+click = [3, 0]
+
+new_board = updateBoard(board, click)
+
+for row in new_board:
+    print(row)                          # ['B', '1', 'E', '1', 'B']
+                                        # ['B', '1', 'M', '1', 'B']
+                                        # ['B', '1', '1', '1', 'B']
+                                        # ['B', 'B', 'B', 'B', 'B']
