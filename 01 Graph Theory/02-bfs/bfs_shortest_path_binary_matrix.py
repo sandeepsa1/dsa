42b92e7e6323eb28f09701585302da8a220c2246
sandeepsa1 <sandeepsasidharan@gmail.com>
2025-11-15 01:05:50 +0530

    BFS based shortest path of a Binary Matrix

--- 01 Graph Theory/02-bfs/bfs_shortest_path_binary_matrix.py
+++ 01 Graph Theory/02-bfs/bfs_shortest_path_binary_matrix.py
@@ -1,6 +1,33 @@
 from collections import deque
 
 def shortestPathBinaryMatrix(grid):
+    n = len(grid)
+    
+    if grid[0][0] != 0 or grid[n-1][n-1] != 0:
+        return -1
+    
+    directions = [
+        (1, 0), (-1, 0), (0, 1), (0, -1),
+        (1, 1), (1, -1), (-1, 1), (-1, -1)
+    ]
+    
+    queue = deque()
+    queue.append((0, 0, 1)) 
+    grid[0][0] = 1
+    
+    while queue:
+        r, c, dist = queue.popleft()
+        
+        if r == n-1 and c == n-1:
+            return dist
+        
+        for dr, dc in directions:
+            nr, nc = r+dr, c+dc
+            
+            if 0 <= nr < n and 0 <= nc < n and grid[nr][nc] == 0:
+                grid[nr][nc] = 1
+                queue.append((nr, nc, dist+1))
+    
     return -1
 
 
@@ -10,4 +37,4 @@ grid = [
   [1, 0, 0]
 ]
 res = shortestPathBinaryMatrix(grid)
-print (res)
+print (res) # 3
