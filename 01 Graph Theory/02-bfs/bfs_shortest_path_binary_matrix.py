a0637b8f6bbc745c96b433cc706798b4141b9cc7
sandeepsa1 <sandeepsasidharan@gmail.com>
2025-11-14 17:14:15 +0530

    BFS based shortest path of a Binary Matrix

---
+++ 01 Graph Theory/02-bfs/bfs_shortest_path_binary_matrix.py
@@ -0,0 +1,13 @@
+from collections import deque
+
+def shortestPathBinaryMatrix(grid):
+    return -1
+
+
+grid = [
+  [0, 1, 0],
+  [0, 0, 0],
+  [1, 0, 0]
+]
+res = shortestPathBinaryMatrix(grid)
+print (res)
