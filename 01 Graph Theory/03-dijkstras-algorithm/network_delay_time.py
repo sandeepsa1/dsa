394ce5954d670e6d67db8511a6e9736c81223755
sandeepsa1 <sandeepsasidharan@gmail.com>
2025-12-19 23:16:57 +0530

    Network Delay Time using Dijkstras Algorithm

---
+++ 01 Graph Theory/03-dijkstras-algorithm/network_delay_time.py
@@ -0,0 +1,16 @@
+import heapq
+from collections import defaultdict
+
+def networkDelayTime(times, n, k):    
+    return ''
+
+
+times = [
+    [2, 1, 1],
+    [2, 3, 1],
+    [3, 4, 1]
+]
+n = 4
+k = 2
+
+print(networkDelayTime(times, n, k))
